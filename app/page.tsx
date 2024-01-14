35d9fab4c3aa1e563941a9a2681d1082e5f89427
Zanges <3868054+Zanges@users.noreply.github.com>
2024-01-14 22:16:13 +0100

    Update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p>
-      hi
-    </p>
+    <main 
+      className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-cyan-900 to-gray-900"
+    >
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-bold text-white drop-shadow-md">
+          Raid Signup
+        </h1>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Login
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   )
 }
