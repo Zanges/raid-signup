04e593fb2902f8561ca5d03e2edd98f27ef1e0ad
Zanges <3868054+Zanges@users.noreply.github.com>
2024-01-15 23:54:59 +0100

    Refactor social component to include a separator and improve layout

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,18 +2,31 @@
 
 import { BiLogoDiscord } from "react-icons/bi";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export function Social() {
   return (
-    <div className="flex items-center w-full gap-x-2 justify-center">
-      <Button
-        variant="ghost"
-        size="icon"
-        className="flex items-center justify-center w-14 h-14"
-        onClick={() => {}}
-      >
-        <BiLogoDiscord size={52} color="rgb(88, 101, 242)" />
-      </Button>
+    <div className="flex flex-col items-center w-full gap-y-2">
+      <div className="flex items-center w-full gap-x-2 justify-center">
+        <Separator orientation="horizontal" className="w-20 bg-muted-foreground/70" />
+        <span className="text-md font-medium text-muted-foreground">
+          OR
+        </span>
+        <Separator orientation="horizontal" className="w-20 bg-muted-foreground/70" />
+      </div>
+      <span className="text-sm font-medium text-muted-foreground">
+        Sign in with
+      </span>
+      <div className="flex items-center w-full gap-x-2 justify-center">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="flex items-center justify-center w-14 h-14"
+          onClick={() => {}}
+        >
+          <BiLogoDiscord size={52} color="rgb(88, 101, 242)" />
+        </Button>
+      </div>
     </div>
   );
 }
