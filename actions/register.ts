b0d8232da903a2a58fbcd3f595befb206ee7ffee
Zanges <3868054+Zanges@users.noreply.github.com>
2024-01-16 20:16:52 +0100

    Add user registration functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import bcrypy from "bcrypt";
 import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedValues = RegisterSchema.safeParse(values);
@@ -12,6 +15,27 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
       error: "Invalid values!"
     };
   }
+
+  const {
+    email,
+    username,
+    password
+  } = validatedValues.data;
+  const hashedPassword = await bcrypy.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email is already in use!" }
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      username,
+      password: hashedPassword
+    },
+  });
 
   return {
     success: "Registering!"
